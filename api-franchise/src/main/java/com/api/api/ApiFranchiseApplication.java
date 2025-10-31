76566e4de3b35e2ff828f223c850164a11c36958
cristian gil <45280035+cristiangil28@users.noreply.github.com>
2025-10-31 12:02:04 -0500

    initial commit

---
+++ api-franchise/src/main/java/com/api/api/ApiFranchiseApplication.java
@@ -0,0 +1,13 @@
+package com.api.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFranchiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFranchiseApplication.class, args);
+	}
+
+}
